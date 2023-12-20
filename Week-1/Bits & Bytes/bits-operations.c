b57bfba13fbff7a82cf411146d5ee41a32cbf6e1
It-Is-Ishank <ishank2086.be21@chitkara.edu.in>
2023-12-20 23:21:18 +0530

    uploaded bits & bytes section

---
+++ Week-1/Bits & Bytes/bits-operations.c
@@ -0,0 +1,38 @@
+int bitAnd(int x, int y) {
+    return ~(~x | ~y);
+}
+
+int bitXor(int x, int y) {
+    return ~(~x & ~y); 
+}
+
+int sign(int x) {
+    int signBit = x >> 31;
+    return (!!x) | signBit;
+}
+
+int getByte(int x, int n){
+    int bytePos = n << 3;
+    int mask = 255;
+    return (x >> bytePos) & mask;
+}
+
+int logicalShift(int x,int n){
+    int mask = ~(((1 << 31) >> n) >> 1);
+    return (x >> n) & mask;
+}
+
+int conditional(int x,int y,int z){
+    int temp = (x >> 31) | ((~x + 1) >> 31);
+    return ((temp & y) + (~temp & z));
+}
+
+int bang(int x) {
+    int res = (((~x + 1) | x) >> 31);
+    return res + 1;
+}
+
+int invert(int x, int p, int n) {
+    int mask = ((1 << n) - 1) << p;
+    return x ^ mask;
+}
